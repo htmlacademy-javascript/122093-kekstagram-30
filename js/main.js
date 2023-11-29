cc8fd8e76226c2ca1686e43728867631c62b623f
Dula53 <kad.keeper@gmail.com>
2023-11-30 01:40:49 +0600

    11.14. Надо подкачаться

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-import { data } from './data.js';
-import {renderPictures} from'./miniature.js';
 import './popup.js';
 import './form.js';
 import './imageEditing.js';
+import {renderPictures} from './miniature';
+import {loadPictures} from './api.js';
+import {showDataErroreMessande} from './message.js';
 
-renderPictures(data);
+const bootstrap = async () => {
+  try {
+    const pictures = await loadPictures();
+    renderPictures(pictures);
+  } catch {
+    showDataErroreMessande();
+  }
+};
+
+bootstrap();
